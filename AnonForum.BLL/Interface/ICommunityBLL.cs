9c7d5721f02eef78bff3aba0f75358f6c9e5b868
ilhams16 <58805531+ilhams16@users.noreply.github.com>
2024-03-14 21:04:53 +0700

    Added Community DAL

---
+++ AnonForum.BLL/Interface/ICommunityBLL.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AnonForum.BLL.DTOs.Community;
+using AnonForum.BLL.DTOs.Post;
+
+namespace AnonForum.BLL.Interface
+{
+	public interface ICommunityBLL
+	{
+		AddNewUser(AddUserCommunityDTO addUserCommunityDTO);
+	}
+}
---
+++ AnonForum.DAL/Interface/ICommunity.vb
@@ -0,0 +1,6 @@
+﻿Imports AnonForum.BO
+
+Public Interface ICommunity
+    Function GetAll() As IEnumerable(Of Community)
+    Function GetByID(id As Integer)
+End Interface
