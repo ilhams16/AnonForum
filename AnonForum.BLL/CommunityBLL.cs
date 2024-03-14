0edbd79191a5a55d2ccc4a7e65162ccce3192ae1
ilhams16 <58805531+ilhams16@users.noreply.github.com>
2024-03-14 20:59:59 +0700

    Added Community DAL

---
+++ AnonForum.BLL/CommunityBLL.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AnonForum.BLL.DTOs.Community;
+using AnonForum.BLL.DTOs.User;
+using AnonForum.BLL.Interface;
+using AnonForum.BO;
+using AnonForum.DAL;
+
+namespace AnonForum.BLL
+{
+	public class CommunityBLL : ICommunityBLL
+	{
+		private readonly ICommunity _communityDAL;
+		private readonly IUser _userDAL;
+		public CommunityBLL()
+		{
+			_communityDAL = new CommunityDAL();
+			_userDAL = new UserDAL();
+		}
+		public void AddNewUser(AddUserCommunityDTO addUserCommunityDTO)
+		{
+			try
+			{
+				_userDAL.add
+			}
+			catch (Exception ex)
+			{
+				throw new ArgumentException(ex.Message);
+			}
+		}
+	}
+
+    internal interface ICommunity
+    {
+    }
+}
